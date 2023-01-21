85033ad5e11fc8b790d7ffde2c57d010dad28975
dan-lay <mr.danlay@gmail.com>
2023-01-21 10:06:10 -0500

    videos work so put them back

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,3 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	
-/******/ 	
-/******/ })()
-;
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c9dcad491533193afe73ec967254fed3b877edd05cc192fb3c5e7c33f577028
+size 662
